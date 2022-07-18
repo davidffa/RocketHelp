b2757271b4fffb4f06e6dd170f08aad70eb6916e
David Amorim <davidfilipeamorim@gmail.com>
2022-07-18 16:03:04 +0100

    Add states in SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -7,7 +8,14 @@ import { Input } from '../components/Input';
 import Logo from '../assets/logo_primary.svg';
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const { colors } = useTheme();
+
+  function handleSignIn() {
+    console.log(email, password);
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -21,15 +29,19 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        value={email}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        value={password}
+        onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button title="Entrar" w="full" onPress={handleSignIn} />
     </VStack>
   )
 }
